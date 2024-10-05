2034e5e303ce5c05d92994816b997e9a71c252ef
HarisRaa <naufal.122140040@student.itera.ac.id>
2024-10-06 00:19:13 +0700

    fix

--- task_2/JS/login.js
+++
@@ -1,8 +0,0 @@
-function handleSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const queryString = new URLSearchParams(formData).toString();
-    window.location.href = `Profile.html?${queryString}`;
-    return false;
-}
--- task_2/JS/profile.js
+++
@@ -1,5 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-document.getElementById('nama').textContent = urlParams.get('nama') || 'Tidak tersedia';
-document.getElementById('email').textContent = urlParams.get('email') || 'Tidak tersedia';
-document.getElementById('usia').textContent = urlParams.get('usia') || 'Tidak tersedia';
-document.getElementById('pekerjaan').textContent = urlParams.get('pekerjaan') || 'Tidak tersedia';
